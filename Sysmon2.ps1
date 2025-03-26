43a796655a4b354eafdadf35edf58870e111bf14
Ajay-V-Git <ajayvernekar1212@gmail.com>
2025-03-26 18:20:05 +0530

    Add files via upload

---
+++ Sysmon2.ps1
@@ -0,0 +1,64 @@
+﻿<#
+.SYNOPSIS
+Install-Sysmon downloads the Sysmon executables archive and installs Sysmon64.exe
+with a configuration file.
+.DESCRIPTION
+PowerShell script or module to install Sysmon with configuration 
+.PARAMETER path
+The path to the working directory.  Default is user Documents.
+.EXAMPLE
+Install-Sysmon -path C:\Temp
+#>
+
+[CmdletBinding()]
+
+#Establish parameters for path
+param (
+    $path = "C:\Temp"
+)
+
+#Test path and create it if required
+
+If(!(test-path $path))
+{
+	Write-Information -MessageData "Path does not exist.  Creating Path..." -InformationAction Continue;
+	New-Item -ItemType Directory -Force -Path $path | Out-Null;
+	Write-Information -MessageData "...Complete" -InformationAction Continue
+}
+
+Set-Location $path
+
+Write-Host "Location set $path"
+
+Write-Host "Retrieving Sysmon..."
+
+Invoke-WebRequest -Uri https://download.sysinternals.com/files/Sysmon.zip -Outfile Sysmon.zip
+
+Write-Host "Sysmon Retrived"
+
+Write-Host "Unzip Sysmon..."
+
+Expand-Archive Sysmon.zip -Force
+
+Set-Location $path\Sysmon
+
+Write-Host "Unzip Complete."
+
+Write-Host "Retrieving Configuration File..."
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/Cyber74-LLC/sysmon-modular/master/c74prod.xml -Outfile sysmonconfig-export.xml
+
+Write-Host "Configuration File Retrieved."
+
+Write-Host "Installing Sysmon..."
+
+$Parameter1= "-accepteula -i sysmonconfig-export.xml"
+$Parameter2= "-accepteula -c sysmonconfig-export.xml"
+
+Start-Process sysmon.exe -ArgumentList $Parameter1 -PassThru -ErrorAction Stop
+Start-Process sysmon.exe -ArgumentList $Parameter1 -PassThru -ErrorAction Stop
+
+#.\sysmon.exe 
+#.\sysmon.exe -accepteula -c sysmonconfig-export.xml
+
+Write-Host "Sysmon Installed!"
